f553fcec8feca382359a221745305958dd524b44
Yu Feng <rainwoodman@gmail.com>
2013-02-18 08:31:20 -0500

    update the glib install script

--- install-glib.sh
+++ install-glib.sh
@@ -5,6 +5,6 @@ wget http://ftp.gnome.org/pub/gnome/sources/glib/2.30/glib-2.30.2.tar.bz2
 tar -xjvf glib-2.30.2.tar.bz2
 (
   cd glib-2.30.2
-  ./configure --prefix=$PWD/../GLIB-STATIC --enable-static --disable-shared
+  ./configure --prefix=$PWD/../GLIB-STATIC --enable-static --disable-shared --enable-debug=yes
   (cd glib; make install )
 )
