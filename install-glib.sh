30d22e12e78de5b42930051b3198ddfddc126bc1
Yu Feng <yfeng1@coma.hpc1.cs.cmu.edu>
2013-03-11 00:58:43 -0400

    skip LIBFFI as glib/glib doesn't need it

--- install-glib.sh
+++ install-glib.sh
@@ -5,6 +5,6 @@ wget http://ftp.gnome.org/pub/gnome/sources/glib/2.30/glib-2.30.2.tar.bz2
 tar -xjvf glib-2.30.2.tar.bz2
 (
   cd glib-2.30.2
-  ./configure --prefix=$PWD/../GLIB-STATIC --enable-static --disable-shared --enable-debug=yes
+  ./configure --prefix=$PWD/../GLIB-STATIC --enable-static --disable-shared --enable-debug=yes LIBFFI_CFLAGS=-DFFI LIBFFI_LIBS=-L/usr/lib64
   (cd glib; make install )
 )
